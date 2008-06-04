81b143d1437ea3ef8a74c2788c5afb9ecf2865e0
Will Riley <wanderwill@gmail.com>
2008-06-04 19:54:02 +0000

    moved the functionality from plugin to controller

--- controllers/SimpleContactFormController.php
+++ controllers/SimpleContactFormController.php
@@ -2,69 +2,69 @@
 /**
 * SimpleContactFormController
 */
-class SimpleContactFormController extends Omeka_Controller_Action
-{	
-	public function init() {
-		$this->session = new Zend_Session_Namespace('SimpleContactForm');
-		$this->_modelClass = 'SimpleContactFormEntry';		
-	}
-		
+class SimpleContactFormController extends Omeka_Controller_Action {
+			
 	public function addAction() {
-		$item = new Item;
-		return $this->renderContributeForm($item);		
+		if (!empty($_POST)) {
+			$this->submitAction();
+		}
+		return $this->render('simplecontactform/add.php');					
 	}
 	
 	public function thankyouAction() {
 		$this->render('simplecontactform/thankyou.php');
 	}
 	
-	protected function renderContributeForm($item) {
-		return $this->render('simplecontactform/add.php', compact('item'));		
-	}
-	
 	/**
-	 * Final submission redirect to a thank-you page
+	 * add entry and redirect to a thank-you page
 	 *
 	 * @return void
 	 **/
+	
 	public function submitAction() {		
-		$session = $this->session;
-		$item = $session->item;
-		$item->name = $_POST['name'];
-		$item->message = $_POST['message'];
-		$item->email = $_POST['email'];
-		$item->save();
-		
-		$this->sendEmailNotification($item);
-		
-		$this->_redirect('contribution/thankyou');
+		//$db = get_db();
+		$message = $_POST['message'];
+		$email = $_POST['email'];
+		$name = $_POST['name'];
+	
+		if (array_key_exists('email', $_POST)) {
+	
+			$entry = new SimpleContactFormEntry();
+	
+			if(!empty($text)) {
+				$entry->message = $message; 
+				$entry->email = $email;
+				$entry->name = $name;
+				$this->sendEmailNotification($entry);
+				$this->_redirect('simple-contact-form/thankyou'); //change for .10 version
+			} 
+		}		
 	}
 	
 	protected function sendEmailNotification($entry) {
+		
 		//notify the admin
 		//use the admin email specified in the plugin configuration.  
-		//if that does not exist, then use the admin email specified for the site.
-		$to_email = get_option('simple_contact_form_notification_email');
+		$to_email = get_option('simple_contact_form_forward_to_email');
 		if (empty($to_email)) {
 			$to_email = get_option('administrator_email'); // use the admin email defined in the site
+			$from_email = $entry->email; //the user's email address
+			$body = "A user has contacted the admin at " . get_option('site_title') . " with the following message:\n\n" . $entry->message;
+			$title = "User Has Contacted  " . get_option('site_title');
+			$header = "From: " . $from_email . "\r\n" .'X-Mailer: PHP/' . phpversion();
+			$res = mail( $to_email, $title, $body, $header);
 		}
-		$from_email = $item->email; //the user's email address
-		$body = "A user has contacted the admin at " . get_option('site_title') . " with the following message:\n\n" . $item->message;
-		$title = "User Has Contacted  " . get_option('site_title');
-		$header = "From: " . $from_email . "\r\n" .'X-Mailer: PHP/' . phpversion();
-		$res = mail( $to_email, $title, $body, $header);
-	
+		
 		//notify the user who sent the message
-		$from_email = get_option('simple_contact_form_notification_email');   
-		$to_email = $item->email; // the user's email address
+		$from_email = get_option('simple_contact_form_reply_to_email');   
+		$to_email = $entry->email; // the user's email address
 		//If this field is empty, don't send the email
-		if(empty($from_email)) {
-			return;
+		if(empty($from_email)) {	
+			$body = "Thank you for contacting " . get_option('site_title') . " with the following message:\n\n" . $item->message;
+			$title = "Thank You For Contacting " . get_option('site_title');
+			$header = "From: " . $from_email . "\r\n" .'X-Mailer: PHP/' . phpversion();
+			$res = mail($to_email, $title, $body, $header);		
 		}
-		$body = "Thank you for contacting " . get_option('site_title') . " with the following message:\n\n" . $item->message;
-		$title = "Thank You For Contacting " . get_option('site_title');
-		$header = "From: " . $from_email . "\r\n" .'X-Mailer: PHP/' . phpversion();
-		$res = mail($to_email, $title, $body, $header);		
 	}
 }
  
--- plugin.php
+++ plugin.php
@@ -20,18 +20,9 @@ add_plugin_hook('config', 'simple_contact_form_config');
 add_plugin_hook('install', 'simple_contact_form_install');
 
 function simple_contact_form_install() {
-	
-	$db = get_db();
-
-	$db->exec("CREATE TABLE IF NOT EXISTS {$db->prefix}simple_contact_form (
-		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-		`message` TEXT NOT NULL,
-		`email` VARCHAR(100) NOT NULL,
-		`name` TEXT NOT NULL ,
-		INDEX ( `id` )
-		) ENGINE = MYISAM");
-	
 	set_option('simple_contact_form_version', SIMPLE_CONTACT_FORM_VERSION);	
+	set_option('simple_contact_form_reply_to_email', get_option('administrator_email'));
+	set_option('simple_contact_form_forward_to_email', get_option('administrator_email'));	
 }
 
 function simple_contact_form_initialize() {
@@ -39,66 +30,28 @@ function simple_contact_form_initialize() {
 	add_theme_pages('theme', 'public');
 }
 
-function simple_contact_form_entry() {
-	$db = get_db(); 
-	
-	$entry = new SimpleContactFormEntry(); // Like a row in the database.
-	
-	return $entry;
+function simple_contact_form_routes($router) {
+	 
+	$router->addRoute('contact', new Zend_Controller_Router_Route('contact', array('controller'=>'simple-contact-form', 'action'=>'add')));
 }
 
-function contribution_routes($router)
-{ 
-	$router->addRoute('contact', new Zend_Controller_Router_Route('contact/', array('controller'=>'simplecontactform', 'action'=>'add')));
-}
-
-function save_simple_contact_form_entry($entry) {
-	
-	$db = get_db();
-	
-	$message = $_POST['message'];
-	$email = $_POST['email'];
-	$name = $_POST['name'];
-	
-	if (array_key_exists('email', $_POST)) {
-		
-		$entry = simple_contact_form_entry();
-				
-		if(!empty($text)) {
-										
-			$entry->message = $message; 
-			$entry->email = $email;
-			$entry->name = $name;
-			$entry->save();
-	
-		} 
-	}	
-}
-
-function simple_contact_form_html() {
-	
-	$html .= '<form name="simple_contact_form" method="post">';
-	$html .= '<label for="message">Your Message:</label>';
-	$html .= '<textarea id="message" name="message" value=""></textarea>';
-	$html .= text(array('name'=>'name', 'id'=>'name', 'class'=>'textinput'), 'Name:');
-	$html .= text(array('name'=>'email', 'id'=>'email', 'class'=>'textinput'), 'Email:');
-	$html .= submit();
-	$html .= '</form>';
-	
-	return $html;
-	
-}
-
-function simple_contact_form_config_form()
-{
+function simple_contact_form_config_form() {
         ?>
-	<label for="simple_contact_form_notification_email">SimpleContactForm Notificaton Email Address:</label><p class="instructionText">Please enter the email address that you would like to appear in the 'From' field for all notification emails for new contact submissions.  Leave this field blank if you would not like to email a user  whenever he/she makes a submission.</p>
-	
-	<input type="text" name="simple_contact_form_notification_email" value="<?php settings('simple_contact_form_notification_email'); ?>" />
+		<label for="simple_contact_form_forward_to_email">Forward-To Email Address:</label>
+		<p class="instructionText">Please enter the email address to where you would like to forward all notification emails from users who have contacted you.  Leave this field blank if you would not like to receive notifications when users contact you.</p>
+		<input type="text" name="simple_contact_form_forward_to_email" value="<?php echo settings('simple_contact_form_forward_to_email'); ?>" />
+		<br/>
+		<br/>
+		<label for="simple_contact_form_reply_to_email">Reply-To Email Address:</label>
+		<p class="instructionText">Please enter the email address that you would like to appear in the 'From' field for all notification emails to users who have contacted you.  Leave this field blank if you would not like to email confirmations to users when they contact you.</p>
+		<input type="text" name="simple_contact_form_reply_to_email" value="<?php echo settings('simple_contact_form_reply_to_email'); ?>" />
+		<br/>
+	    <br/>
 	<?php
 }
 
-function simple_contact_form_config($post)
-{
-	set_option('simple_contact_form_notification_email', $post['simple_contact_form_notification_email']);
+function simple_contact_form_config($post) {
+	$post = $_POST;
+	set_option('simple_contact_form_forward_to_email', $post['simple_contact_form_forward_to_email']);
+	set_option('simple_contact_form_reply_to_email', $post['simple_contact_form_reply_to_email']);
 }
