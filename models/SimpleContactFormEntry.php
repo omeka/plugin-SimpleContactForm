378e99a44b0a94081aab6306b16ab26a781eb028
Will Riley <wanderwill@gmail.com>
2008-06-04 19:52:28 +0000

    renamed the model

---
+++ models/SimpleContactFormEntry.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+* SimpleContactFormEntry 
+*
+* @author CHNM
+* @copyright CHNM, 2 June, 2009
+* @package: Omeka 
+*
+*/
+
+class SimpleContactFormEntry { 
+	// in the future extends Omeka_Record
+	public $email;
+	public $name;
+	public $message;
+}
+?>
