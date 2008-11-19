2a044a4fcb56365d0e01f9f5cb5db01ad38515a1
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-11-19 15:37:51 +0000

    Fixes markup structure on SimpleContactForm scripts.

--- views/public/index/index.php
+++ views/public/index/index.php
@@ -1,25 +1,49 @@
 <?php head(); ?>
-<h1><?php echo get_option('simple_contact_form_contact_page_title'); ?></h1>
+<h1><?php echo htmlspecialchars(get_option('simple_contact_form_contact_page_title')); ?></h1>
+<div id="primary">
+    
 <div id="simplecontactform">
 	<div id="simplecontactforminstructions">
 		<?php echo nls2p(get_option('simple_contact_form_contact_page_instructions')); ?>
 	</div>
 	<?php echo flash(); ?>
 	<form name="simple_contact_form" id="simple_contact_form"  method="post" enctype="multipart/form-data" accept-charset="utf-8">
-
+        
+        <fieldset>
+            
+        <div class="field">
 		<?php 
-			echo text(array('name'=>'name', 'id'=>'name', 'class'=>'textinput'), $name, 'Your Name: '); 
-		 ?>
-		<?php
-			echo text(array('name'=>'email', 'id'=>'email', 'class'=>'textinput'), $email, 'Your Email: ');
-		?>
-		<br/>
-
-		<label for="message">Your Message:</label>
-		<textarea id="message" name="message"><?php echo $message; ?></textarea>
+		    echo $this->formLabel('name', 'Your Name: ');
+		    echo $this->formText('name', $name, array('class'=>'textinput')); ?>
+		</div>
 		
-		<?php echo $captcha; ?>
-	<?php echo submit('Send Message'); ?>
+        <div class="field">
+            <?php 
+            echo $this->formLabel('email', 'Your Email: ');
+		    echo $this->formText('email', $email, array('class'=>'textinput'));  ?>
+        </div>
+        
+		<div class="field">
+		  <?php 
+		    echo $this->formLabel('message', 'Your Message: ');
+		    echo $this->formTextarea('message', $message, array('class'=>'textinput')); ?>
+		</div>    
+		
+		</fieldset>
+		
+		<fieldset>
+		    
+		<div class="field">
+		  <?php echo $captcha; ?>
+		</div>		
+		
+		<div class="field">
+		  <?php echo submit('Send Message'); ?>
+		</div>
+	    
+	    </fieldset>
 	</form>
+</div>
+
 </div>
 <?php foot(); ?>
--- views/public/index/thankyou.php
+++ views/public/index/thankyou.php
@@ -1,6 +1,8 @@
 <?php head(); ?>
-<h1><?php echo get_option('simple_contact_form_thankyou_page_title'); ?></h1>
-<?php echo nls2p(get_option('simple_contact_form_thankyou_page_message')); ?>
+<h1><?php echo get_option('simple_contact_form_thankyou_page_title'); // Not HTML ?></h1>
 
-<?php print_r($renderVars);?>
+<div id="primary">
+<?php echo nls2p(get_option('simple_contact_form_thankyou_page_message')); // HTML ?>
+</div>
+
 <?php foot(); ?>
