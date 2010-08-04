27a6fc1b05d4d0e40c7662d8c071e0881c69c249
John Flatness <john@zerocrates.org>
2010-08-04 13:38:48 +0000

    Removes obsolete test bootstrap.

--- tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$baseBootstrap = dirname(dirname(dirname(dirname(__FILE__)))) . '/application/tests/bootstrap.php';
-require_once $baseBootstrap;
-require_once 'SimpleContactForm_TestCase.php';
