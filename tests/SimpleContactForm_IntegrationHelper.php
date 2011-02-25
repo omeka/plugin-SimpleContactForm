40bfbb45de57b9bfd10d4770d5c484c1099fd428
Jeremy Boggs <jeremy@clioweb.org>
2011-02-25 13:09:46 +0000

    Corrects value for package name.

--- tests/SimpleContactForm_IntegrationHelper.php
+++ tests/SimpleContactForm_IntegrationHelper.php
@@ -3,7 +3,7 @@
  * @version $Id$
  * @copyright Center for History and New Media, 2010
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @package Geolocation
+ * @package SimpleContactForm
  */
 class SimpleContactForm_IntegrationHelper
 {
