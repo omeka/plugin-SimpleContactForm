55a7ca90024ff27f100414b1c65561b099f2c51b
Darío Hereñú <magallania@gmail.com>
2014-10-01 19:01:01 -0300

    Update copyright years?

--- tests/cases/Test.php
+++ tests/cases/Test.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version $Id$
- * @copyright Center for History and New Media, 2007-2011
+ * @copyright Center for History and New Media, 2007-2014
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package SimpleContactForm
  */
@@ -10,7 +10,7 @@
  * Tests for SimpleContactForm.
  *
  * @package SimpleContactForm
- * @copyright Center for History and New Media, 2007-2011
+ * @copyright Center for History and New Media, 2007-2014
  */
 class SimpleContactForm_Test extends Omeka_Test_AppTestCase {
     
